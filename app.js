74fa912659e32a824f958977ee6c2d3982647278
VanekCheck <mykytynivan04@gmail.com>
2022-03-07 12:24:24 +0200

    First version of programm with second level of security

---
+++ app.js
@@ -0,0 +1,74 @@
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs")
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption")
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost/userDB')
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+})
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] })
+
+const User = mongoose.model("User", userSchema)
+
+app.get("/", (req, res) => {
+   res.render("home");
+});
+
+app.get("/login", (req, res) => {
+   res.render("login");
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    User.findOne({email: username}, (err, foundUser) => {
+      if(!err){
+        if (foundUser){
+          if(foundUser.password === password){
+            res.render("secrets")
+          }else{
+            console.log("Wrong password");
+          }
+        }else{
+          console.log("No such a user");
+        }
+      }
+    })
+
+})
+
+app.get("/register", (req, res) => {
+   res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const registerUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  })
+  registerUser.save(err => {
+    if (!err){
+      res.render("secrets")
+    }else{
+      console.log(err);
+    }
+  })
+})
+
+app.listen(3000, () => {
+ console.log("the server is running at port 3000");
+});
